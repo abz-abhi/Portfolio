05eec651cd51c17e124653862d14dee6621a1d82
Abhishek <abhislinux@gmail.com>
2025-06-10 15:01:16 +0530

    second commit

--- src/pages/page1.jsx
+++ src/pages/page1.jsx
@@ -2,8 +2,11 @@ import Header from "../components/header";
 
 const Mainpage = () => {
   return (
-    <div class="bg-[#11071F]">
+    <div >
       <Header />
+      <div class="bg-[#11071F]">
+        <div class="w-[385px] h-[431px] rounded-full bg-gradient-radial from-white via-[#41078e] to-transparent"></div>
+      </div>
     </div>
   );
 };
